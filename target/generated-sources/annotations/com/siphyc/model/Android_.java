b3f052db2fcde7478e2eb92a3f6367515c97b656
korena <moazkorena@gmail.com>
2016-01-25 04:53:25 +0800

    working state again, unsatisfied dependency still problematic in tests ...

--- target/generated-sources/annotations/com/siphyc/model/Android_.java
+++ target/generated-sources/annotations/com/siphyc/model/Android_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-25T03:31:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-25T04:49:23")
 @StaticMetamodel(Android.class)
 public class Android_ { 
 
