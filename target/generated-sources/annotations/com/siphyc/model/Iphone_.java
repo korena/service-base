2d50ecf83021b2a551ccd17493230fc14f7bddd2
korena <moazkorena@gmail.com>
2016-01-26 12:41:05 +0800

    log files excluded

---
+++ target/generated-sources/annotations/com/siphyc/model/Iphone_.java
@@ -0,0 +1,19 @@
+package com.siphyc.model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-01-26T12:33:13")
+@StaticMetamodel(Iphone.class)
+public class Iphone_ { 
+
+    public static volatile SingularAttribute<Iphone, Date> created;
+    public static volatile SingularAttribute<Iphone, String> model;
+    public static volatile SingularAttribute<Iphone, Integer> id;
+    public static volatile SingularAttribute<Iphone, Date> updated;
+    public static volatile SingularAttribute<Iphone, String> customer;
+    public static volatile SingularAttribute<Iphone, Boolean> status;
+
+}
